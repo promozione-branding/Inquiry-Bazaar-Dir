70b605227fb48337d675ef86c7a04847caf55b68
Anshuman <anshutyagi799@gmail.com>
2026-05-07 11:05:16 +0530

    fix

--- src/components/Main/Popup.jsx
+++ src/components/Main/Popup.jsx
@@ -14,7 +14,7 @@ export default function Popup({ open, setOpen, details }) {
         const formData = new FormData(e.target);
         const data = {
             supplierToken: details?.supplier?._id,
-            platform: "Bar Bending Machine - Promote Bharat Dir Portfolio Form",
+            platform: "Promote Bharat Dir Portfolio Popup",
             platformEmail: "shreeshaktiinfratech@gmail.com",
             name: formData.get("contactPerson"),
             email: formData.get("email"),
