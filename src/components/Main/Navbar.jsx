7c5ca44c0b893aa77ed5a3653c5369e66bdd3c95
Anshuman <anshutyagi799@gmail.com>
2026-06-25 18:30:53 +0530

    fixes

--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -198,9 +198,66 @@ export default function Navbar() {
                     <MapPin size={20} />
                 </button>
 
-                <button className="md:hidden bg-[#f45a06] px-3 py-2 rounded-md" onClick={() => setOpen(!open)}>
-                    {open ? <X size={20} /> : <Menu size={20} />}
-                </button>
+                {user ?
+                    <div ref={profileRef} className="relative">
+                        <button onClick={() => setProfileOpen(!profileOpen)}
+                            className="flex items-center gap-3 border rounded-full hover:bg-gray-50 border-gray-200"
+                        >
+                            <Image
+                                src={user.profileImage || "/profile.png"}
+                                alt={user.name}
+                                width={40}
+                                height={40}
+                                className="rounded-full object-cover w-22 h-12"
+                            />
+                        </button>
+
+                        {profileOpen && (
+                            <div className="absolute right-0 mt-1 w-72 bg-white rounded-xl shadow-xl border border-gray-200 overflow-hidden">
+                                <div className="px-4 py-2 border-b border-gray-200">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <h3 className="font-semibold text-black">
+                                            {user.name}
+                                        </h3>
+
+                                        <p className="text-sm text-gray-800">
+                                            {user.email}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="p-2">
+                                    {user.role == "supplier" && <>
+                                        <Link href="https://seller.inquirybazaar.com/profile"
+                                            className="px-4 py-3 rounded-lg hover:bg-gray-100 text-black flex gap-2 items-center"
+                                        >
+                                            <User size={18} className="-mt-0.5" />  Profile
+                                        </Link>
+                                        <Link href="https://seller.inquirybazaar.com/dashboard"
+                                            className="px-4 py-3 rounded-lg hover:bg-gray-100 text-black flex gap-2 items-center"
+                                        >
+                                            <Layers size={18} className="-mt-0.5" />  Dashboard
+                                        </Link>
+                                    </>}
+                                    {user.role == "buyer" && <>
+                                        <Link href="https://buyer.inquirybazaar.com/profile"
+                                            className="px-4 py-3 rounded-lg hover:bg-gray-100 text-black flex gap-2 items-center"
+                                        >
+                                            <User size={18} className="-mt-0.5" />  Profile
+                                        </Link>
+                                        <Link href="https://buyer.inquirybazaar.com/dashboard"
+                                            className="px-4 py-3 rounded-lg hover:bg-gray-100 text-black flex gap-2 items-center"
+                                        >
+                                            <Layers size={18} className="-mt-0.5" />  Dashboard
+                                        </Link>
+                                    </>}
+                                </div>
+                            </div>
+                        )}
+                    </div> :
+                    <button className="md:hidden bg-[#f45a06] px-3 py-2 rounded-md" onClick={() => setOpen(!open)}>
+                        {open ? <X size={20} /> : <Menu size={20} />}
+                    </button>}
             </div>
 
             {open && (
@@ -251,6 +308,49 @@ export default function Navbar() {
                                 setShowDropdown={setShowDropdown}
                                 handleFocus={handleFocus}
                             />
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {locationOpen && (
+                <div className="fixed inset-0 z-50">
+                    {/* Backdrop */}
+                    <div className="absolute inset-0 bg-black/50" />
+
+                    {/* Modal Content */}
+                    <div className="relative flex justify-center">
+                        <button
+                            onClick={() => setLocationOpen(false)}
+                            className="absolute top-5 right-5 text-white hover:text-black"
+                        >
+                            <X size={28} />
+                        </button>
+                        <div className="absolute top-[20vh] bg-wite px-4 pb-4 rounded-lg shadow-lg w-full max-w-xl">
+
+                            {/* Close Button */}
+
+                            <div className="flex relative">
+                                <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 text-[#f45a06] pointer-events-none" size={18} />
+
+                                <select onChange={(e) => { dispatch(setLocation(e.target.value)); (pathname.startsWith("/category") || pathname.startsWith("/search")) && handleSelect(e) }} value={location}
+                                    className=" w-full appearance-none rounded-xl border border-orange-300 bg-white py-2.5 pl-10 pr-10
+      text-sm font-medium text-gray-700 shadow-sm transition-all duration-200 outline-none hover:border-[#f45a06] focus:border-[#f45a06] focus:ring-1 focus:ring-orange-100 cursor-pointer">
+                                    {/* <option value="All India">
+                                All India
+                            </option> */}
+
+                                    {locations.flatMap((state) =>
+                                        state.cities.map((city) => (
+                                            <option key={`${state.state}-${city}`} value={city}>
+                                                {city}
+                                            </option>
+                                        ))
+                                    )}
+                                </select>
+
+                                <ChevronDown size={18} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-800 pointer-events-none" />
+                            </div>
                         </div>
                     </div>
                 </div>
